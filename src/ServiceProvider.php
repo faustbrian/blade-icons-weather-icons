064630b0c19ef81497869e95a44d42af5cc5864f
Brian Faust <hello@basecode.sh>
2024-11-23 17:30:18 +0200

    first commit
    Signed-off-by: Brian Faust <hello@basecode.sh>

---
+++ src/ServiceProvider.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+/**
+ * Copyright (C) BaseCode Oy - All Rights Reserved
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace BaseCodeOy\BladeIcons\WeatherIcons;
+
+use BaseCodeOy\BladeIcons\Facades\IconFamilyRegistry;
+use BaseCodeOy\BladeIcons\IconFamily;
+use BaseCodeOy\PackagePowerPack\Package\AbstractServiceProvider;
+
+final class ServiceProvider extends AbstractServiceProvider
+{
+    public function packageRegistered(): void
+    {
+        IconFamilyRegistry::push(
+            IconFamily::fromDirectory('weather', __DIR__.'/../resources/svg'),
+        );
+    }
+}
